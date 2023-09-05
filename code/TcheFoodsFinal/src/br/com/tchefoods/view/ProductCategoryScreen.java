291ea115201ea7317cf919228dd0b34a0d3bafb3
Pedro Gabriel Cardoso Faleiro <pedrogabrielcardosofaleiro@gmail.com>
2023-09-04 21:22:24 -0300

    Coloquei tela Produto e Categoria

---
+++ code/TcheFoodsFinal/src/br/com/tchefoods/view/ProductCategoryScreen.java
@@ -0,0 +1,14 @@
+package br.com.tchefoods.view;
+
+import javax.swing.*;
+
+public class ProductCategoryScreen {
+    private JTextField TFProductCategory;
+    private JLabel JLProductCategory;
+    private JLabel JLDescription;
+    private JButton BSubmit;
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ code/TcheFoodsFinal/src/br/com/tchefoods/view/ProductScreen.java
@@ -0,0 +1,19 @@
+package br.com.tchefoods.view;
+
+import javax.swing.*;
+
+public class ProductScreen {
+    private JTextField TFPrice;
+    private JTextField TFDescription;
+    private JLabel JLAddProducts;
+    private JLabel JLPrice;
+    private JLabel JLIdCategory;
+    private JTextField TFIdCategory;
+    private JLabel JLDescription;
+    private JPanel JPSddProducts;
+    private JButton BSubmit;
+
+    public static void main(String[] args) {
+
+    }
+}
