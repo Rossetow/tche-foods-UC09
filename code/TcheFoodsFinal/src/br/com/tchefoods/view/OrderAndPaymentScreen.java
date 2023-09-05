a3e638eb372d8641edb536e4a712a607d19bb825
Thaironn <eldranddn@gmail.com>
2023-09-04 21:51:41 -0300

    tela de pedido e pagamento

---
+++ code/TcheFoodsFinal/src/br/com/tchefoods/view/OrderAndPaymentScreen.java
@@ -0,0 +1,18 @@
+package br.com.tchefoods.view;
+
+import javax.swing.*;
+
+public class OrderAndPaymentScreen {
+    private JPanel PanelOrder;
+    private JTextField TFProductName;
+    private JTextField TFPrice;
+    private JButton ButtonAdd;
+    private JTable TableProducts;
+    private JTextField TFTtotalPrice;
+    private JComboBox CBPaymentMethod;
+    private JButton ButtonBuy;
+    private JLabel JLTitle;
+    private JLabel JLProductName;
+    private JLabel JLPrice;
+    private JLabel JLProductPriceTotal;
+}
