d440bfefb4f2ea232f2863a690dfa65249e78c7e
Thaironn <eldranddn@gmail.com>
2023-09-05 19:28:45 -0300

    Alteração, novamente, da infra connection mysql

--- code/TcheFoodsFinal/src/br/com/tchefoods/infra/ConnectionMysql.java
+++ code/TcheFoodsFinal/src/br/com/tchefoods/infra/ConnectionMysql.java
@@ -2,22 +2,22 @@ package br.com.tchefoods.infra;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-public class ConnectionMysql {
-    private String serverName = "127.0.0.1";
-    private String user = "root";
-    private String password = "senac";
-    private String dataBaseName = "db_tchefoods_final";
+    public class ConnectionMysql {
+        private String serverName = "127.0.0.1";
+        private String user = "root";
+        private String password = "senac";
+        private String dataBaseName = "db_tchefoods_final";
 
-    public Connection obterConexao() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
+        public Connection obterConexao() throws ClassNotFoundException, SQLException {
+            Class.forName("com.mysql.cj.jdbc.Driver");
 
-        String url = "jdbc:mysql://" + this.serverName + ":3306/" + this.dataBaseName;
+            String url = "jdbc:mysql://" + this.serverName + ":3306/" + this.dataBaseName;
 
-        Connection connection = null;
+            Connection connection = null;
 
-        connection = DriverManager.getConnection(url, user, password);
-        return connection;
+            connection = DriverManager.getConnection(url, user, password);
+            return connection;
 
+        }
     }
-}
 
