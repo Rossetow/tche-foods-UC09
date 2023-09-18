6baf2c987d28c72bf7939c9cfdc7d0b5962b2020
Rossetow <rafaelrosseto05@gmail.com>
2023-09-18 00:11:42 -0300

    .

--- code/TcheFoodsFinal/src/br/com/tchefoods/view/ProductScreenView.java
+++ code/TcheFoodsFinal/src/br/com/tchefoods/view/ProductScreenView.java
@@ -1,20 +1,15 @@
 package br.com.tchefoods.view;
 
-import br.com.tchefoods.dao.OrderDAO;
 import br.com.tchefoods.dao.ProductDAO;
-import br.com.tchefoods.dao.UserDAO;
 import br.com.tchefoods.model.*;
-
 import javax.swing.*;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
 import java.sql.SQLException;
 import java.util.List;
 
 
 public class ProductScreenView {
     private JTable JTProduct;
-    private JPanel JPProductScreenView;
+    private JPanel PanelScreenView;
 
     public ProductScreenView() {
         try {
